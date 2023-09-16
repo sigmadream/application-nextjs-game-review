ed4f51398aaa0903274438d17c9941d422b639c6
Sangkon Han <sigmadream@gmail.com>
2023-09-17 01:03:02 +0900

    feat: rendering and nav

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,33 @@
+import Link from "next/link";
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
-        <header>[Header]</header>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/reviews">Reviews</Link>
+              </li>
+              <li>
+                <Link href="/about" prefetch={false}>
+                  About
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <main>{children}</main>
-        <footer>[footer]</footer>
+        <footer>
+          Game dat and images courtesy of{" "}
+          <a href="https://www.rawg.io" target="_blank">
+            RAWG
+          </a>
+        </footer>
       </body>
     </html>
   );
