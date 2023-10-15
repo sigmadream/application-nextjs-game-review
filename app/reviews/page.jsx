7e050bc109c7d5e81b954f17006517c1f84afa15
Sangkon, Han <sigmadream@gmail.com>
2023-10-16 03:34:15 +0900

    feat: CMS에서 Reviews API 호출

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,38 +1,53 @@
-import Link from "next/link";
-import Heading from "@/components/Heading";
-import { getReviews } from "@/lib/reviews";
+import { readdir, readFile } from "node:fs/promises";
+import matter from "gray-matter";
+import { marked } from "marked";
+import qs from "qs";
 
-export const metadata = {
-  title: "Reviews",
-};
-
-export default async function ReviewsPage() {
+export async function getFeaturedReview() {
   const reviews = await getReviews();
-  console.log("[ReviewsPage] reviews:", reviews);
-  return (
-    <>
-      <Heading>Reviews</Heading>
-      <ul className="flex flex-row flex-wrap gap-3">
-        {reviews.map((review) => (
-          <li
-            key={review.slug}
-            className="bg-white border rounded shadow w-80 hover:shadow-xl"
-          >
-            <Link href={`/reviews/${review.slug}`}>
-              <img
-                src={review.image}
-                alt=""
-                width="320"
-                height="180"
-                className="rounded-t"
-              />
-              <h2 className="font-orbitron font-semibold py-1 text-center">
-                {review.title}
-              </h2>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  return reviews[0];
+}
+
+export async function getReview(slug) {
+  const text = await readFile(`./content/reviews/${slug}.md`, "utf8");
+  const {
+    content,
+    data: { title, date, image },
+  } = matter(text);
+  const body = marked(content, { headerIds: false, mangle: false });
+  return { slug, title, date, image, body };
+}
+
+/*
+    slug: 'hellblade',
+    title: 'Hellblade',
+    date: '2023-05-06',
+    image: '/images/hellblade.jpg',
+*/
+export async function getReviews() {
+  const url =
+    "http://127.0.0.1:1337/api/reviews?" +
+    qs.stringify(
+      {
+        fields: ["slug", "title", "subtitle", "publishedAt"],
+        populate: { image: { fields: ["url"] } },
+        sort: ["publishedAt:desc"],
+        pagination: { pageSize: 6 },
+      },
+      { encodeValuesOnly: true }
+    );
+  console.log("getReviews:", url);
+  const response = await fetch(url);
+  const { data } = await response.json();
+  return data.map(({ attributes }) => ({
+    slug: attributes.slug,
+    title: attributes.title,
+  }));
+}
+
+export async function getSlugs() {
+  const files = await readdir("./content/reviews");
+  return files
+    .filter((file) => file.endsWith(".md"))
+    .map((file) => file.slice(0, -".md".length));
 }
