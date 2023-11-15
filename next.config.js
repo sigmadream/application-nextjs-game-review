1bc7541cb61b2cae5dff70bdfbd6ca39465218ac
Sangkon Han <sigmadream@gmail.com>
2023-11-15 23:12:40 +0900

    feat: 배포 준비 끗

--- next.config.js
+++ next.config.js
@@ -2,12 +2,23 @@
 module.exports = {
   images: {
     remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "1337",
-        pathname: "/uploads/**",
-      },
+      // {
+      //   protocol: "http",
+      //   hostname: "127.0.0.1",
+      //   port: "1337",
+      //   pathname: "/uploads/**",
+      // },
+      toRemotePattern(process.env.CMS_IMAGE_PATTERN),
     ],
   },
 };
+
+function toRemotePattern(urlString) {
+  const url = new URL(urlString);
+  return {
+    protocol: url.protocol.replace(":", ""),
+    hostname: url.hostname,
+    port: url.port,
+    pathname: url.pathname,
+  };
+}
